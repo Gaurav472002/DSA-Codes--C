8ad83d186dbda2b8d004f03161702a2651d0eca7
Gaurav Chakraborty <gairavchakraborty472@gmail.com>
2022-03-04 17:03:29 +0530

    made some changes

--- DFS.c
+++ DFS.c
@@ -16,8 +16,7 @@ void DFS(int i){
     printf("%d ", i);
     visited[i] = 1;
     for (int j = 0; j < 7; j++)
-    {
-        
+    { 
         if(A[i][j]==1 && !visited[j]){
             DFS(j);
         }
